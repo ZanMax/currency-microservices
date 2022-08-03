6c24d7000363cb86046942e536bdc4eab81d6b0b
maxim <zanoga.maxim@gmail.com>
2022-08-02 22:56:33 -0700

    init

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+/*
+SERVICES
+https://fixer.io/
+https://currencylayer.com/
+https://exchangeratesapi.io/
+https://currencyapi.com/
+https://www.abstractapi.com/
+https://currency.getgeoapi.com/
+https://openexchangerates.org/
+https://www.exchangerate-api.com/
+*/
+
+func saveHistory(cur string) {
+	fmt.Println(cur)
+}
+
+func backgroundUpdate() {
+	for {
+		cur := getFixerCurrency()
+		fmt.Println(cur)
+		time.Sleep(time.Second * 60)
+	}
+}
+
+func getFixerCurrency() string {
+	apiKey := "YOUR_API_KEY"
+	//url := "http://data.fixer.io/api/latest?access_key=" + apiKey
+	url := fmt.Sprintf("http://data.fixer.io/api/latest?access_key=%s", apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(respBody)
+}
+
+func main() {
+	go backgroundUpdate()
+
+	//cur := getFixerCurrency()
+	//fmt.Println(cur)
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run(":8080")
+}
